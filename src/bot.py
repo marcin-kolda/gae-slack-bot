e8a424e491d1389edca180e7f3a1100e31f955c4
Marcin Kołda <marcin.kolda@gmail.com>
2017-09-24 00:00:01 +0200

    added retrieving bot_id

--- src/bot.py
+++ src/bot.py
@@ -1,15 +1,20 @@
 import logging
 
-class Bot(object):
 
+class Bot(object):
     def __init__(self, slack_client):
         self.slack_client = slack_client
         self.bot_id = self.__get_bot_id()
 
     def __get_bot_id(self):
-        bot_id = self.slack_client.api_call("auth.test")['user_id']
-        logging.info("Retrieved bot_id: %s", bot_id)
-        return bot_id
+        response = self.slack_client.api_call("auth.test")
+        if not response['ok']:
+            logging.error("Unable to retrieve bot id due to %s",
+                          response['error'])
+        else:
+            bot_id = response['user_id']
+            logging.info("Retrieved bot_id: '%s'", bot_id)
+            return bot_id
 
     def handle_event(self, event):
         pass
